296c316b13c2d12de52db5ff881b678af71ff676
viktoriia_ksenofontova <glory_viktory@hotmail.com>
2021-11-24 17:47:39 +0200

    forms

--- js/menu-button.js
+++ js/menu-button.js
@@ -1,18 +1,49 @@
 $(document).ready(function () {
-  $('.menu__button').click(function () {
-    console.log("click");
-  $(".menu__button").toggleClass("is-open");
-  $('.menu__container').toggleClass("is-open");
-    $('.header').toggleClass("is-open");
-    $("body").toggleClass("overflowed");
-  });
-
-  $('.navbar__link').click(function (){
-    $("body").toggleClass("overflowed");
+  
+  function toggleClassIsOpen() {
     $(".menu__button").toggleClass("is-open");
-  $('.menu__container').toggleClass("is-open");
+    $('.menu__container').toggleClass("is-open");
     $('.header').toggleClass("is-open");
-  });
+    $("body").toggleClass("overflowed");
+}
+
+  $('.menu__button').click(toggleClassIsOpen);
+
+  $('.navbar__link').click(toggleClassIsOpen);
+  
+  $('.nav__contacts__telephone').click(toggleClassIsOpen);
+  
+  $('.nav__contacts__button').click(toggleClassIsOpen);
+// Scroll to top logic
+
+  let rootElement = document.documentElement;
+  
+  function scrollToTop() {
+      rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+
+  $('#scrollToTopBtn').click(scrollToTop);
+
+
+  function handleScroll() {
+  
+  if (rootElement.scrollTop > 500 ) {
+    // Show button
+    $('#scrollToTopBtn').addClass("visible");
+    $('#scrollToTopBtn').removeClass("hidden");
+  }
+   else {
+  //    // Hide button
+    $('#scrollToTopBtn').addClass("hidden");
+     $('#scrollToTopBtn').removeClass("visible");
+   }
+  };
+  
+  $( window ).scroll(handleScroll);
+  
 });
 
 
